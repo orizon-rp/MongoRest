949e6a1f5864a81ddf752063c15842a6b8d71d7b
bub.bl <bub.bl@outlook.com>
2024-11-23 22:53:25 +0100

    Remove unused env variables, we use command line environment variable now

--- Program.cs
+++ Program.cs
@@ -3,30 +3,35 @@ using MongoRest.Converters;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region Environment Variables
+
+var connectionString = Environment.GetEnvironmentVariable("MONGO_URL");
+
+if (string.IsNullOrEmpty(connectionString))
+    throw new Exception("MONGO_URL environment variable is not set.");
+
+var dbName = Environment.GetEnvironmentVariable("MONGO_DB_NAME");
+
+if (string.IsNullOrEmpty(dbName))
+    throw new Exception("MONGO_DB_NAME environment variable is not set.");
+
+#endregion
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
-{
-    var connectionString = builder.Configuration.GetValue<string>("MongoDb");
-    return new MongoClient(connectionString);
-});
+builder.Services.AddSingleton<IMongoClient, MongoClient>(_ => new MongoClient(connectionString));
 
 builder.Services.AddScoped(sp =>
 {
-    var dbName = builder.Configuration.GetValue<string>("DatabaseName");
-
     var client = sp.GetRequiredService<IMongoClient>();
     return client.GetDatabase(dbName);
 });
 
 builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new BsonDocumentJsonConverter());
-    });
+    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new BsonDocumentJsonConverter()); });
 
 var app = builder.Build();
 
@@ -39,4 +44,6 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 app.UseHttpsRedirection();
 
-public partial class Program { }
+public partial class Program
+{
+}
