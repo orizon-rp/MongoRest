bddac9f03586995ceb3037c3a21a083638b924cb
bub.bl <bub.bl@outlook.com>
2024-11-23 11:35:44 +0100

    Add a basic way to insert a document in a collection

---
+++ Controllers/CollectionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoRest.Controllers;
+
+[ApiController]
+[Route($"{Constant.APIRootPath}/[controller]")]
+public sealed class CollectionsController(IMongoDatabase database) : ControllerBase
+{
+    /// <summary>
+    /// Creates a new document in the specified collection.
+    /// </summary>
+    /// <param name="collectionName">The name of the collection to create the document in.</param>
+    /// <param name="document">The document to be created.</param>
+    /// <returns>A successful result with a message indicating the document was created successfully.</returns>
+    [HttpPost("{collectionName}/create")]
+    public async Task<IActionResult> CreateAsync(string collectionName, [FromBody] BsonDocument document)
+    {
+        if (string.IsNullOrWhiteSpace(collectionName))
+            return BadRequest("A collection name is required.");
+
+        var collection = database.GetCollection<BsonDocument>(collectionName);
+        await collection.InsertOneAsync(document);
+
+        return Ok(new { message = "Document created successfully." });
+    }
+}
