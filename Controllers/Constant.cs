7f69b18375a41470db4a352787c69994f08da292
bub.bl <bub.bl@outlook.com>
2024-11-23 11:33:08 +0100

    Store api root path as constant

---
+++ Controllers/Constant.cs
@@ -0,0 +1,7 @@
+﻿namespace MongoRest.Controllers;
+
+public static class Constant
+{
+    public const string CurrentAPIVersion = "v1";
+    public const string APIRootPath = $"/api/{CurrentAPIVersion}";
+}
