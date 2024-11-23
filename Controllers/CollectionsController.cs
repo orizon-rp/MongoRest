1e1ccf4259f1e98448b23ae89d8c45d52f7d6fef
bub.bl <bub.bl@outlook.com>
2024-11-23 11:39:41 +0100

    we can get a record from its id (its fine for now, but we need to improve this later with a custom filter system)

--- Controllers/CollectionsController.cs
+++ Controllers/CollectionsController.cs
@@ -25,11 +25,40 @@ public sealed class CollectionsController(IMongoDatabase database) : ControllerB
             var collection = database.GetCollection<BsonDocument>(collectionName);
             await collection.InsertOneAsync(document);
 
-            return Ok(new { message = "Document created successfully." });   
+            return Ok(new { message = "Document created successfully." });
         }
         catch (Exception ex)
         {
             return StatusCode(500, new { message = "Insertion failed.", error = ex.Message });
         }
+    }
+
+    /// <summary>
+    /// Retrieves a document from the specified collection by id.
+    /// </summary>
+    /// <param name="collectionName">The name of the collection to retrieve the document from.</param>
+    /// <param name="id">The id of the document to be retrieved.</param>
+    /// <returns>A successful result with the document if found, otherwise a 404 not found error.</returns>
+    [HttpGet("{collectionName}")]
+    public async Task<IActionResult> GetAsync(string collectionName, [FromQuery] string? id = null)
+    {
+        if (string.IsNullOrWhiteSpace(collectionName))
+            return BadRequest("A collection name is required.");
+
+        var collection = database.GetCollection<BsonDocument>(collectionName);
+
+        if (id is not null)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+            var document = await collection.Find(filter).FirstOrDefaultAsync();
+
+            if (document is null)
+                return NotFound(new { message = "Document not found." });
+
+            return Ok(document);
+        }
+
+        var documents = await collection.Find(Builders<BsonDocument>.Filter.Empty).ToListAsync();
+        return Ok(documents);
     }
 }
