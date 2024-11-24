699f412c870f9fce89a64d6778e2ad4f82a41eb1
bub.bl <bub.bl@outlook.com>
2024-11-24 16:23:59 +0100

    Switch between args and environment vars depending on the platform

--- Program.cs
+++ Program.cs
@@ -1,19 +1,51 @@
+using System.Runtime.InteropServices;
 using MongoDB.Driver;
 using MongoRest.Converters;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = string.Empty;
+var dbName = string.Empty;
+
 #region Environment Variables
 
-var connectionString = Environment.GetEnvironmentVariable("MONGO_URL");
+void EnsureArgs()
+{
+    var options = args.Select(arg => arg.Split('=')).ToDictionary(split => split[0].Trim('-'), split => split[1]);
 
-if (string.IsNullOrEmpty(connectionString))
-    throw new Exception("MONGO_URL environment variable is not set.");
+    options.TryGetValue("MONGO_URL", out connectionString);
+    options.TryGetValue("MONGO_DB_NAME", out dbName);
 
-var dbName = Environment.GetEnvironmentVariable("MONGO_DB_NAME");
+    if (string.IsNullOrEmpty(connectionString))
+        throw new Exception("MONGO_URL command line argument is not set.");
 
-if (string.IsNullOrEmpty(dbName))
-    throw new Exception("MONGO_DB_NAME environment variable is not set.");
+    if (string.IsNullOrEmpty(dbName))
+        throw new Exception("MONGO_DB_NAME command line argument is not set.");
+}
+
+void EnsureEnv()
+{
+    connectionString = Environment.GetEnvironmentVariable("MONGO_URL")!;
+    dbName = Environment.GetEnvironmentVariable("MONGO_DB_NAME")!;
+
+    if (string.IsNullOrEmpty(connectionString))
+        throw new Exception("MONGO_URL environment variable is not set.");
+
+    if (string.IsNullOrEmpty(dbName))
+        throw new Exception("MONGO_DB_NAME environment variable is not set.");
+}
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    EnsureArgs();
+}
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    // If the app is running in a docker container
+    if (File.Exists("/.dockerenv")) EnsureEnv();
+    // If the app is running locally
+    else EnsureArgs();
+}
 
 #endregion
 
@@ -43,6 +75,8 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 app.UseHttpsRedirection();
+
+app.Run();
 
 public partial class Program
 {
